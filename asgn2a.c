6289f7c479f9a7154b831f10cfe339a79f3d3b28
potechtohk <fe7ng654123@gmail.com>
2021-03-19 15:05:21 +0800

    pass without threads

--- asgn2a.c
+++ asgn2a.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
+#include<string.h>
 #include<unistd.h>
+#include <omp.h>
 #include"util.h"
 
 // If you have referenced to any source code that is not written by you
@@ -19,13 +21,72 @@ int asgn2a(Point * points, Point ** pPermissiblePoints, int number, int dim, int
 	Point * permissiblePoints = NULL;
 
 	//the following for-loop iterates the first 20 points that will be inputted by runtest.c
-    //	for(int i = 0; i < 20; i++)
-    //		printPoint(points[i], dim);
+    
+    // for(int i = 0; i < 20; i++)
+    //     printPoint(points[i], dim);
 
 	/**********************************************************************************
 	 * Work here
 	 * *******************************************************************************/
-	
+    printf("\n--------------start---------------\n\n");
+	// printf("points->ID = %d, length = %ld \n",
+    //     points->ID, sizeof(points->values));
+    // for (int i = 0; i < sizeof(points->values); i++)
+    // {
+    //     printf("value[%d] = %f\n", i, points->values[i]);
+    // }
+    omp_set_num_threads(2);
+    int counter = 0;
+    int flag = 1;
+
+    // #pragma omp parallel for reduction(+:permissiblePointNum)
+    for (int i = 0; i < number; i++)
+    {
+        // #pragma omp atomic 
+        flag = 1;
+        counter = 0;
+        for (int j = 0; j < number; j++)
+        {
+            if(i==j)
+                continue;
+            // #pragma omp atomic 
+            counter = 0;
+            for (int k = 0; k < dim; k++)
+            {   // omp reduction here???
+                // printf(" i j k = %d %d %d\n", i , j , k);  
+                if (points[i].values[k] > points[j].values[k]){
+                    // #pragma omp atomic
+                    counter++;
+                    // printf("ID %d counter = %d\n",i+1, counter);
+                }
+            }
+            if (counter>=dim){
+                // #pragma omp atomic 
+                flag=0;
+                // printf("ID %d prevails ID %d, ID %d is kicked out\n", j+1, i+1, i+1);
+            } 
+            if (flag ==0)
+            {
+                break;
+            }
+            
+        }
+
+        if(flag){
+            // printf("ID %d survived\n", i+1);
+            // #pragma omp atomic
+            permissiblePointNum++;
+            // printf("permissiblePointNum = %d\n", permissiblePointNum);
+            permissiblePoints= realloc(permissiblePoints, permissiblePointNum*sizeof(Point));
+            memcpy(&permissiblePoints[permissiblePointNum-1],&points[i], sizeof(Point));
+        }
+        
+    }
+    
+
+    printf("\n--------------end---------------\n\n");
+
+
     
     // pPermissiblePoints -- your computed answer
 	*pPermissiblePoints = permissiblePoints;
