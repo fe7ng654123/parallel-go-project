e557f81bea6349cf80c383dfa3ea4bb4d6f39357
potechtohk <fe7ng654123@gmail.com>
2021-04-01 10:47:49 +0800

    complete split_four

--- asgn2a.c
+++ asgn2a.c
@@ -11,6 +11,155 @@
 // If you have referenced to any source code that is not written by you
 // You have to cite them here.
 
+union Point_U{
+    __m128 m128;
+    __m256 m256;
+    float v[8];
+};
+
+struct  InThreadPara
+{
+    int counter; //4 bytes
+    int padding[13]; //52 bytes
+    Point * points; //8 bytes  
+    //total 64 byte, fit in a cacheline
+};
+
+int split_four(Point *points, int number, int dim, Point *points_subset ){
+
+    struct InThreadPara* inTPara =  malloc(sizeof(struct InThreadPara)*4);
+
+    for (int i = 0; i < 4; i++)
+    {
+        inTPara[i].points = malloc(number/4*sizeof(Point));
+        memcpy(&inTPara[i].points[0],&points[number/4*i], sizeof(Point));
+        inTPara[i].counter =1;
+    }
+
+    #pragma omp parallel for num_threads(4) schedule(static, number/4) //reduction(+:permissiblePointNum )
+    for (int i = 0; i < number; i++)
+    {
+        if( (i%(number/4)) == 0) continue;
+
+        int flag = 1;
+        int i_inserted = 0;
+        int j_wins_index = -1;
+        
+        Point * tmpPoints = malloc(number*sizeof(Point));
+        int index = 0;
+        
+        int tid = i/(number/4);
+        // printf("i,tid = %d %d\n",i,tid);
+        // compare candidta with known permissible points
+        for (int j = 0; j < inTPara[tid].counter; j++)
+        {
+            // printf("waaaaaaaaaaaaaaaaaa");
+
+            int counter = 0; //for counting numbers of greater of equal dimensions
+            union Point_U result_m; //for storing the bool table when comparing 2 points
+
+            if (dim ==5){
+            __m256 point1 = _mm256_setr_ps(
+                points[i].values[0],
+                points[i].values[1],
+                points[i].values[2],
+                points[i].values[3],
+                points[i].values[4],
+                0,0,0); 
+            __m256 point2 = _mm256_setr_ps(
+                inTPara[tid].points[j].values[0],
+                inTPara[tid].points[j].values[1],
+                inTPara[tid].points[j].values[2],
+                inTPara[tid].points[j].values[3],
+                inTPara[tid].points[j].values[4],
+                0,0,0); 
+            __m256 bool_table = _mm256_cmp_ps(point1,point2,_CMP_GE_OS);
+            result_m.m256 = _mm256_and_ps(bool_table,_mm256_set1_ps(1));
+            }else{
+                __m128 point1 = _mm_load_ps(points[i].values);
+                __m128 point2 = _mm_load_ps(inTPara[tid].points[j].values);
+                __m128 bool_table = _mm_cmp_ps(point1,point2,_CMP_GE_OS);
+                result_m.m128 = _mm_and_ps(bool_table, _mm_set1_ps(1));
+            }
+            for (int i = 0; i < dim; i++)
+            {
+                counter += (int)result_m.v[i];
+            }
+
+            if (counter>=dim){ //all dimensions of point i are greater than or equal to point j
+                flag = 0;
+                // break;
+                // printf("jjjjjjjjjjjjjjjj wins i =%d j =%d\n", i,j);
+                memcpy(&tmpPoints[index],&inTPara[tid].points[j], sizeof(Point));
+                index++;
+                j_wins_index =j;
+                break;
+                // can ignore the later ones and directly copy
+            } else if(counter==0 ){
+                // printf("iiiiiiiiiii wins i =%d j =%d\n", i,j);
+                flag = 0;
+                if (i_inserted ==0)
+                {
+                   memcpy(&tmpPoints[index],&points[i], sizeof(Point));
+                    index++;
+                }
+                i_inserted =1;
+            } else{
+                // printf("no one wins\n");
+                memcpy(&tmpPoints[index],&inTPara[tid].points[j], sizeof(Point));
+                index++;
+            }
+            
+        }
+
+        if(j_wins_index != -1){
+            for (int j = (j_wins_index+1); j < inTPara[tid].counter; j++)
+            {
+                memcpy(&tmpPoints[index],&inTPara[tid].points[j], sizeof(Point));
+                index++;
+            }
+        }
+
+        if(flag){
+            memcpy(&inTPara[tid].points[inTPara[tid].counter],&points[i], sizeof(Point));
+            inTPara[tid].counter++;
+        } else{
+            free(inTPara[tid].points);
+            inTPara[tid].points = tmpPoints;
+            inTPara[tid].counter=index;
+        }
+
+        // for (int i = 0; i < permissiblePointNum; i++)
+        // {
+        //     printf("id = %d ", permissiblePoints[i].ID);
+        // }
+        // printf("\n");
+        
+        
+        
+    }
+
+    // Point * points_second = malloc(number*sizeof(Point));
+    int points_subset_num = 0;
+    
+    for (int i = 0; i < 4; i++)
+    {
+        printf("inTPara[%d].counter = %d\n",i,inTPara[i].counter); 
+        // permissiblePointNum+=inTPara[i].counter;
+
+        for (int j = 0; j < inTPara[i].counter; j++)
+        {
+            memcpy(&points_subset[points_subset_num],&inTPara[i].points[j], sizeof(Point));
+            points_subset_num++;
+        }
+        
+    }
+
+    printf("points_subset_num = %d \n", points_subset_num);
+    return points_subset_num;
+
+};
+
 int asgn2a(Point * points, Point ** pPermissiblePoints, int number, int dim, int thread_number)
 {
     // points -- input data
@@ -31,53 +180,28 @@ int asgn2a(Point * points, Point ** pPermissiblePoints, int number, int dim, int
 	 * *******************************************************************************/
     printf("\n--------------start---------------\n\n");
 
-    union Point_U{
-        __m128 m128;
-        __m256 m256;
-        float v[8];
-    };
-
 
     permissiblePoints= realloc(permissiblePoints, number*sizeof(Point));
 
-    struct __attribute__((aligned (64))) InThreadBuff
-    {
-        int counter; //4 bytes
-        int padding[13]; //52 bytes
-        Point * permissiblePoints; //8 bytes  
-        //total 64 byte, fit in a cacheline
-    };
 
-    // struct InThreadBuff* inTbuff =  malloc(sizeof(struct InThreadBuff)*4);
-    // for (int i = 0; i < 4; i++)
-    // {
-    //     inTbuff[i].counter=0;
-    //     inTbuff[i].permissiblePoints = malloc((number/4)*sizeof(Point));
-    //     printf("inTbuff[i] = %p\n", &inTbuff[i]);
-    // }
+    Point * points_second = malloc(number*sizeof(Point));
+    int points_second_num = 0;
+
+    points_second_num = split_four(points,number,dim,points_second);
+
+    printf("points_second_num = %d \n", points_second_num);
+    
 
     permissiblePointNum =1;
-    permissiblePoints[0].ID =1;
-    permissiblePoints[0].values = points[0].values;
+    memcpy(&permissiblePoints[0],&points_second[0], sizeof(Point));
 
-    // Point ** sub_result_points[4];
-    // for (int i = 0; i < 4; i++)
-    // {
-    //     sub_result_points[i] = malloc(number/4*sizeof(Point));
-    //     sub_result_points
-    // }
-    
-    
-
-    // #pragma omp parallel for num_threads(4) schedule(dynamic, number/4) //reduction(+:permissiblePointNum )
-    for (int i = 1; i < number; i++)
+    for (int i = 1; i < points_second_num; i++)
     {
-
         int flag = 1;
         int i_inserted = 0;
         int j_wins_index = -1;
         
-        Point * tmpPoints = malloc(number*sizeof(Point));
+        Point * tmpPoints = malloc(points_second_num*sizeof(Point));
         int index = 0;
         
         // compare candidta with known permissible points
@@ -89,11 +213,11 @@ int asgn2a(Point * points, Point ** pPermissiblePoints, int number, int dim, int
 
             if (dim ==5){
             __m256 point1 = _mm256_setr_ps(
-                points[i].values[0],
-                points[i].values[1],
-                points[i].values[2],
-                points[i].values[3],
-                points[i].values[4],
+                points_second[i].values[0],
+                points_second[i].values[1],
+                points_second[i].values[2],
+                points_second[i].values[3],
+                points_second[i].values[4],
                 0,0,0); 
             __m256 point2 = _mm256_setr_ps(
                 permissiblePoints[j].values[0],
@@ -105,7 +229,7 @@ int asgn2a(Point * points, Point ** pPermissiblePoints, int number, int dim, int
             __m256 bool_table = _mm256_cmp_ps(point1,point2,_CMP_GE_OS);
             result_m.m256 = _mm256_and_ps(bool_table,_mm256_set1_ps(1));
             }else{
-                __m128 point1 = _mm_load_ps(points[i].values);
+                __m128 point1 = _mm_load_ps(points_second[i].values);
                 __m128 point2 = _mm_load_ps(permissiblePoints[j].values);
                 __m128 bool_table = _mm_cmp_ps(point1,point2,_CMP_GE_OS);
                 result_m.m128 = _mm_and_ps(bool_table, _mm_set1_ps(1));
@@ -129,7 +253,7 @@ int asgn2a(Point * points, Point ** pPermissiblePoints, int number, int dim, int
                 flag = 0;
                 if (i_inserted ==0)
                 {
-                   memcpy(&tmpPoints[index],&points[i], sizeof(Point));
+                   memcpy(&tmpPoints[index],&points_second[i], sizeof(Point));
                     index++;
                 }
                 i_inserted =1;
@@ -150,12 +274,12 @@ int asgn2a(Point * points, Point ** pPermissiblePoints, int number, int dim, int
         }
 
         if(flag){
-            memcpy(&permissiblePoints[permissiblePointNum],&points[i], sizeof(Point));
+            memcpy(&permissiblePoints[permissiblePointNum],&points_second[i], sizeof(Point));
             permissiblePointNum++;
         } else{
             free(permissiblePoints);
             permissiblePoints = tmpPoints;
-            permissiblePointNum=index;
+            permissiblePointNum = index;
         }
 
         // for (int i = 0; i < permissiblePointNum; i++)
@@ -164,17 +288,8 @@ int asgn2a(Point * points, Point ** pPermissiblePoints, int number, int dim, int
         // }
         // printf("\n");
         
-    
-        
-    }
-    
 
-    // for (int i = 0; i < 4; i++)
-    // {
-    //     printf("inTbuff[%d].counter = %d\n",i,inTbuff[i].counter); 
-    //     permissiblePointNum+=inTbuff[i].counter;
-    //     // inTbuff[i].permissiblePoints = malloc((number/4)*sizeof(Point));
-    // }
+    }
 
     printf("final permissiblePointNum = %d\n", permissiblePointNum);
     
